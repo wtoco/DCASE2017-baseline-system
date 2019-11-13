0b48c0eb894452280832ecaac188c601a3740992
wtoco <358279984@qq.com>
2019-11-13 17:41:38 +0800

    test

--- examples/keras_seq.py
+++ examples/keras_seq.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# DCASE 2017::Detection of rare sound events / Baseline System
+# DCASE 2017::Detection of rare sound events / Baseline System1
 
 
 from __future__ import print_function, absolute_import
